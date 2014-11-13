85956a39b249e1d97a0e6cf5ddfc8ed34800dd49
andrei-pl <andrey.pld@gmail.com>
2014-11-13 21:55:30 +0200

    seed admin user and roles

--- AiDiDesign/AiDiDesign.Data/Migrations/Configuration.cs
+++ AiDiDesign/AiDiDesign.Data/Migrations/Configuration.cs
@@ -5,6 +5,8 @@ namespace AiDiDesign.Data.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
     using AiDiDesign.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
 
     public sealed class Configuration : DbMigrationsConfiguration<AiDiDesignDBContext>
     {
@@ -16,16 +18,51 @@ namespace AiDiDesign.Data.Migrations
 
         protected override void Seed(AiDiDesignDBContext context)
         {
+            //var users = context.Users.ToArray();
             if (context.Users.Any())
             {
                 return;
             }
 
-            context.Users.Add(new User
+            if (!context.Roles.Any(r => r.Name == "admin"))
             {
-                UserName = "Admin",
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole { Name = "admin" };
 
-            });
+                manager.Create(role);
+            }
+
+            if (!context.Roles.Any(r => r.Name == "user"))
+            {
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                var role = new IdentityRole { Name = "user" };
+
+                manager.Create(role);
+            }
+
+            if (!context.Users.Any(u => u.UserName == "aididizain@abv.bg"))
+            {
+                var store = new UserStore<User>(context);
+                var manager = new UserManager<User>(store);
+                var user = new User
+                {
+                    UserName = "aididizain@abv.bg",
+                    Email = "aididizain@abv.bg",
+                    EmailConfirmed = true,
+                };
+
+
+                manager.Create(user, "aididizain");
+                manager.AddToRole(user.Id, "admin");
+            }
+
+            //context.Users.Add(new User
+            //{
+
+
+            //});
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
