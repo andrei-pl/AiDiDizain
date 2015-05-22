57a45e0167d7f7da8cf2d7aba4831600b3e17ae3
andrei-pl <andrey.pld@gmail.com>
2015-05-22 23:29:31 +0300

    Again from the beginning
    - Deleted everythimg
    - Initialization new project
    - Deployed in Azure
    - ApplicationUser and DbContext moved respectively in Data and Models
    Projects
    - Common project with global constants created
    - NuGet packages updated

---
+++ AiDiDesign/Common/AiDiDesign.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace AiDiDesign.Common
+{
+    public class GlobalConstants
+    {
+        public const string DefaultRole = "User";
+        public const string AdminRole = "Administrator";
+        public const string DefaultUserPicture = "https://img-w.zeebox.com/images/profile/generic/Empty_Profile_04.png";
+    }
+}
---
+++ AiDiDesign/Data/AiDiDesign.Data/AiDiDesignDbContext.cs
@@ -0,0 +1,20 @@
+﻿namespace AiDiDesign.Data
+{
+    using System;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    using AiDiDesign.Data.Models;
+
+    public class AiDiDesignDbContext : IdentityDbContext<User>
+    {
+        public AiDiDesignDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static AiDiDesignDbContext Create()
+        {
+            return new AiDiDesignDbContext();
+        }
+    }
+}
